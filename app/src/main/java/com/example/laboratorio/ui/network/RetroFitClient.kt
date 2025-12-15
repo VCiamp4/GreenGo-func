91129111ac8e1b721ea5b39ce8d6502e9511105c
Valentin Ciamparella <ciamparellar@gmail.com>
2025-12-15 14:12:27 -0300

    arreglado front, todo testeado

--- app/src/main/java/com/example/laboratorio/ui/network/RetroFitClient.kt
+++ app/src/main/java/com/example/laboratorio/ui/network/RetroFitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     // Emulador Android → tu PC
-    private const val BASE_URL = "http://10.0.2.2:8000"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
